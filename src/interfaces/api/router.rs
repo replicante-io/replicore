bf0a597c4f6fcdd92b2ca7572076d2b265e5e69b
Stefano Pogliani <stefano@spogliani.net>
2018-03-24 17:15:17 +0000

    Add tests for API interface router

--- src/interfaces/api/router.rs
+++ src/interfaces/api/router.rs
@@ -45,3 +45,53 @@ impl RouterBuilder {
         self.route(method::Get, glob, handler, route_id)
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use iron::Headers;
+    use iron::IronResult;
+    use iron::Request;
+    use iron::Response;
+
+    use iron::method;
+    use iron::status;
+
+    use iron_test::request;
+    use iron_test::response;
+
+    use super::RouterBuilder;
+
+
+    fn mock_get(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "GET")))
+    }
+
+    fn mock_put(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "PUT")))
+    }
+
+    #[test]
+    fn attach_get() {
+        let mut builder = RouterBuilder::new();
+        builder.get("/", &mock_get, "test");
+        let router = builder.build();
+
+        let response = request::get("http://host:16016/", Headers::new(), &router).unwrap();
+        let result_body = response::extract_body_to_bytes(response);
+        let result_body = String::from_utf8(result_body).unwrap();
+        assert_eq!(result_body, "GET");
+    }
+
+    #[test]
+    fn attach_route() {
+        let mut builder = RouterBuilder::new();
+        builder.route(method::Put, "/", &mock_put, "test");
+        let router = builder.build();
+
+        let response = request::put("http://host:16016/", Headers::new(), "", &router).unwrap();
+        let result_body = response::extract_body_to_bytes(response);
+        let result_body = String::from_utf8(result_body).unwrap();
+        assert_eq!(result_body, "PUT");
+    }
+}
--- src/interfaces/api/routes.rs
+++ src/interfaces/api/routes.rs
@@ -9,3 +9,22 @@ use iron::status;
 pub fn root_index(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Replicante API server")))
 }
+
+
+#[cfg(test)]
+mod tests {
+    use iron::Headers;
+    use iron_test::request;
+    use iron_test::response;
+
+    use super::root_index;
+
+
+    #[test]
+    fn get_index() {
+        let response = request::get("http://host:16016/", Headers::new(), &root_index).unwrap();
+        let result_body = response::extract_body_to_bytes(response);
+        let result_body = String::from_utf8(result_body).unwrap();
+        assert_eq!(result_body, "Replicante API server");
+    }
+}
