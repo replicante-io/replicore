c3bcb8785be660eed2b078d2e4142f2a8c350eec
Stefano Pogliani <stefano@spogliani.net>
2019-04-13 23:58:57 +0100

    Test self introspection endpoint

--- replicante/src/interfaces/api/routes/introspect/self_endpoint.rs
+++ replicante/src/interfaces/api/routes/introspect/self_endpoint.rs
@@ -30,24 +30,30 @@ impl ::iron::Handler for Handler {
 
 #[cfg(test)]
 mod tests {
-//    use iron::Chain;
-//    use iron::Headers;
-//    use iron_test::request;
-//    use iron_test::response;
-//    use iron_json_response::JsonResponseMiddleware;
-//
-//    use super::handler;
-//
-//    #[test]
-//    fn get_index() {
-//        let mut chain = Chain::new(&handler);
-//        chain.link_after(JsonResponseMiddleware::new());
-//        let response = request::get("http://host:16016/", Headers::new(), &chain).unwrap();
-//        let result_body = response::extract_body_to_bytes(response);
-//        let result_body = String::from_utf8(result_body).unwrap();
-//        assert_eq!(result_body, format!(
-//            r#"{{"commit":"{}","taint":"{}","version":"{}"}}"#,
-//            env!("GIT_BUILD_HASH"), env!("GIT_BUILD_TAINT"), env!("CARGO_PKG_VERSION")
-//        ));
-//    }
+    use iron::Chain;
+    use iron::Headers;
+    use iron_test::request;
+    use iron_test::response;
+    use iron_json_response::JsonResponseMiddleware;
+    use slog::Discard;
+    use slog::Logger;
+
+    use replicante_coordinator::mock::MockCoordinator;
+
+    use super::Handler;
+
+    #[test]
+    fn get() {
+        let coordinator = MockCoordinator::new(Logger::root(Discard, o!()));
+        let handler = Handler::new(coordinator.mock());
+        let mut chain = Chain::new(handler);
+        chain.link_after(JsonResponseMiddleware::new());
+        let response = request::get("http://host:16016/", Headers::new(), &chain).unwrap();
+        let result_body = response::extract_body_to_bytes(response);
+        let result_body = String::from_utf8(result_body).unwrap();
+        assert_eq!(result_body, format!(
+            r#"{{"extra":{{}},"id":"{}"}}"#,
+            coordinator.node_id
+        ));
+    }
 }
