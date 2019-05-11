a2af1542fe638a9a763b0d83bdfb459f154a332d
Stefano Pogliani <stefano@spogliani.net>
2019-05-11 13:31:35 +0100

    Optional local sentry-in-compose instance

--- devtools/initialise.sh
+++ devtools/initialise.sh
@@ -4,3 +4,10 @@ set -e
 
 # Initialise MongoDB.
 docker-compose exec mongo /replicore/init.sh
+
+
+# Sentry is optional (and also takes forever so do it last).
+if docker-compose ps sentry-web > /dev/null 2>&1; then
+  echo "==> Sentry DB init/sync ..."
+  docker-compose exec sentry-web sentry upgrade
+fi
