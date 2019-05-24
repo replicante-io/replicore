3754a2f4b68e360d6fab77b918f2c5d063cf2b97
Stefano Pogliani <stefano@spogliani.net>
2019-05-25 00:05:43 +0100

    Fix clippy for mongodb crate

--- ci/travis/build-script.sh
+++ ci/travis/build-script.sh
@@ -4,8 +4,9 @@ set -ex
 cargo build
 cargo test
 
-# mongodb crate has an invalid clippy config on newer clippy.
-cargo clippy --all --exclude mongodb -- -D warnings
+# mongodb crate has an invalid clippy config on newer clippy versions.
+sed -i 's/cyclomatic-complexity-threshold/cognitive-complexity-threshold/' ~/.cargo/registry/src/github.com-1ecc6299db9ec823/mongodb-0.3.12/clippy.toml
+cargo clippy -- -D warnings
 
 # Run cargo fmt in verbose mode to see what the editions are.
 cargo fmt --verbose -- --check
