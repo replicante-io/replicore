a4842c178f037318f71c259ff5d4f79a17bd4796
Stefano Pogliani <stefano@spogliani.net>
2019-05-25 00:26:39 +0100

    Allow clippy warnings about lint renames

--- ci/travis/build-script.sh
+++ ci/travis/build-script.sh
@@ -4,9 +4,14 @@ set -ex
 cargo build
 cargo test
 
-# mongodb crate has an invalid clippy config on newer clippy versions.
-sed -i 's/cyclomatic-complexity-threshold/cognitive-complexity-threshold/' ~/.cargo/registry/src/github.com-1ecc6299db9ec823/mongodb-0.3.12/clippy.toml
-cargo clippy -- -D warnings
+# lint `clippy::cyclomatic_complexity` has been renamed to `clippy::cognitive_complexity`
+# but clippy for rust 1.34 does not allow the new name yet.
+# Allow lint renames until the minimum supported rust version is 1.35 and then patch mongodb:
+# ```
+#  sed -i 's/cyclomatic-complexity-threshold/cognitive-complexity-threshold/' \
+#    ~/.cargo/registry/src/github.com-1ecc6299db9ec823/mongodb-0.3.12/clippy.toml
+# ```
+cargo clippy -- -D warnings -A renamed-and-removed-lints
 
 # Run cargo fmt in verbose mode to see what the editions are.
 cargo fmt --verbose -- --check
