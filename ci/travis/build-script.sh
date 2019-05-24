6c8d1037d8130d871957b690ee9f4c8b53b98f6c
Stefano Pogliani <stefano@spogliani.net>
2019-05-24 23:29:49 +0100

    Fix clippy for extrenal crate

--- ci/travis/build-script.sh
+++ ci/travis/build-script.sh
@@ -3,6 +3,9 @@ set -ex
 
 cargo build
 cargo test
-cargo clippy -- -D warnings
+
+# mongodb crate has an invalid clippy config on newer clippy.
+cargo clippy --all --exclude mongodb -- -D warnings
+
 # Run cargo fmt in verbose mode to see what the editions are.
 cargo fmt --verbose -- --check
