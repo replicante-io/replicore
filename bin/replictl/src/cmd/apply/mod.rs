1d47f19ecd6756f783c02b59d602831ef3be601a
Stefano Pogliani <stefano@spogliani.net>
2023-12-24 10:39:22 +0000

    feat: RepliCtl recuse into directories to apply

--- bin/replictl/src/cmd/apply/mod.rs
+++ bin/replictl/src/cmd/apply/mod.rs
@@ -9,15 +9,69 @@ mod transform;
 use crate::context::ContextStore;
 use crate::Globals;
 
+/// File glob for JSON files.
+const JSON_GLOB: &str = "*.json";
+
+/// File glob for YAML files.
+const YAML_GLOB: &str = "*.yaml";
+
+/// Alternative file glob for YAML files.
+const YML_GLOB: &str = "*.yml";
+
 /// Apply objects declarations to the Control Plane.
 #[derive(Debug, Parser)]
 pub struct ApplyCli {
     /// List of files to load manifests to apply from.
     pub file: Vec<String>,
 
+    /// Expand paths to apply recursively instead of expanding only one level into directories.
+    #[arg(short, long, default_value_t = false)]
+    pub recursive: bool,
+
     /// Override scope (namespace, cluster and node) in manifests using context information.
     #[arg(long, default_value_t = false)]
     pub scope_override: bool,
+}
+
+/// Explore directories looking for files to apply.
+fn glob_for_extension(
+    files: &mut Vec<String>,
+    path: &str,
+    extension: &str,
+    recurse: bool,
+) -> Result<()> {
+    let recursive = if recurse { "**" } else { "" };
+    let pattern = format!("{}{}/{}", path, recursive, extension);
+    for path in glob::glob(&pattern)? {
+        let path = path?
+            .to_str()
+            .ok_or_else(|| anyhow::anyhow!("expanded path not valid unicode"))?
+            .to_string();
+        files.push(path);
+    }
+    Ok(())
+}
+
+/// Expand provided directories to supported files within them.
+async fn process_directories(files: Vec<String>, recurse: bool) -> Result<Vec<String>> {
+    let expanded = tokio::task::spawn_blocking(move || -> Result<_> {
+        let mut expanded = Vec::new();
+        for entry in files.into_iter() {
+            // Check if the path points to a discovery.
+            let path = std::path::Path::new(&entry);
+            if !path.is_dir() {
+                expanded.push(entry);
+                continue;
+            }
+
+            // If so expand it to look for all files in it.
+            glob_for_extension(&mut expanded, &entry, YAML_GLOB, recurse)?;
+            glob_for_extension(&mut expanded, &entry, YML_GLOB, recurse)?;
+            glob_for_extension(&mut expanded, &entry, JSON_GLOB, recurse)?;
+        }
+        Ok(expanded)
+    }).await??;
+    Ok(expanded)
 }
 
 /// Execute the selected `replictl apply` command.
@@ -26,8 +80,8 @@ pub async fn run(globals: &Globals, cmd: &ApplyCli) -> Result<i32> {
     let client = crate::client(&context)?;
 
     // If no files are given read from standard input.
-    let mut files = cmd.file.clone();
-    // TODO: support resolving directories into lists of files.
+    let files = cmd.file.clone();
+    let mut files = process_directories(files, cmd.recursive).await?;
     if files.is_empty() {
         files.push("-".into());
     }
